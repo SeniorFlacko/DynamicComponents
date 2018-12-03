ede2f58851de573a5b71c5a2482fb094d0c67d92
Cristian <calemanfuentes@amarello.com.mx>
2018-12-03 16:19:24 -0600

    Add and remove ANY (Generic) Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { 
+  Component, 
+  ViewChild, 
+  ViewContainerRef, 
+  ComponentFactoryResolver, 
+  Type } from '@angular/core';
+import { FooComponent } from './foo/foo.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <!-- Pass the component class as an argument to add and remove based on the component class -->
+  <button (click)="addComponent(fooComponentClass)">Add</button>
+  <button (click)="removeComponent(fooComponentClass)">Remove</button>
+
+  <div>
+    <!-- Use ng-template to ensure that the generated components end up in the right place -->
+    <ng-template #container>
+
+    </ng-template>
+  </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'vcr';
+
+  @ViewChild('container', {read: ViewContainerRef}) container: ViewContainerRef;
+
+  // Keep track of list of generated components for removal purposes
+  components = [];
+
+  // Expose class so that it can be used in the template
+  fooComponentClass = FooComponent;
+
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) {
+  }
+
+  addComponent(componentClass: Type<any>) {
+    // Create component dynamically inside the ng-template
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(componentClass);
+    const component = this.container.createComponent(componentFactory);
+
+    // Push the component so that we can keep track of which components are created
+    this.components.push(component);
+  }
+
+  removeComponent(componentClass: Type<any>) {
+    // Find the component
+    const component = this.components.find((component) => component.instance instanceof componentClass);
+    const componentIndex = this.components.indexOf(component);
+
+    if (componentIndex !== -1) {
+      // Remove component from both view and array
+      this.container.remove(this.container.indexOf(component));
+      this.components.splice(componentIndex, 1);
+    }
+  }
 }
---
+++ src/app/foo/foo.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-foo',
+  template: `
+    <h1>Foobar</h1>
+  `
+})
+export class FooComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
